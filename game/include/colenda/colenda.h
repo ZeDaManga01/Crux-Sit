c7464492b412aa2805745273205ad9143c08d041
Pierre Machado <pierrenovaes@gmail.com>
2024-07-21 20:47:00 -0300

    ♻️ refactor: mudança de macros

--- game/include/colenda/colenda.h
+++ game/include/colenda/colenda.h
@@ -20,10 +20,11 @@
 #define ECLOSE EIO // Input/output error (error closing file)
 #define EWRITE EIO // Input/output error (error writing to file)
 
-#define SCREEN_HEIGHT 480
 #define SCREEN_WIDTH 640
-
+#define SCREEN_HEIGHT 480
 #define BLOCK_SIZE 8
+#define DIVIDED_SCREEN_WIDTH (SCREEN_WIDTH / BLOCK_SIZE)
+#define DIVIDED_SCREEN_HEIGHT (SCREEN_HEIGHT / BLOCK_SIZE)
 #define BUFFER_SIZE 8
 
 int gpuopen(FILE **file, const char *driver_name);
