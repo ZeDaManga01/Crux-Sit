93bdf0153c2e898131b179792228874253b673c9
Pierre Machado <pierrenovaes@gmail.com>
2024-07-21 21:03:56 -0300

    🚨 test: thread e mutex do mouse

--- game/tests/mouse/test.c
+++ game/tests/mouse/test.c
@@ -2,19 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
+
+void *mouse_thread_func(void *arg);
+void *test_thread_func(void *arg);
+
+volatile bool running = true;
+
+pthread_mutex_t mouse_lock = PTHREAD_MUTEX_INITIALIZER;
+mouse_t mouse;
 
 int main(void)
 {
-    mouse_t mouse = mouseinit();
+    mouse = mouseinit();
 
     if (mouse.fd < 0) {
         printf("Erro\n");
         return -1;
     }
 
-    while (1) {
+    pthread_t mouse_thread, test_thread;
+
+    pthread_create(&mouse_thread, NULL, mouse_thread_func, NULL);
+    pthread_create(&test_thread, NULL, test_thread_func, NULL);
+    pthread_join(mouse_thread, NULL);
+    pthread_join(test_thread, NULL);
+    
+    return 0;
+}
+
+void *mouse_thread_func(void *arg)
+{
+    while (running) {
+        pthread_mutex_lock(&mouse_lock);
         mouseread(&mouse);
         updatemousesensitivity(&mouse, 1, 1);
+        pthread_mutex_unlock(&mouse_lock);
 
         if (mouse.event) {
             printf("Mouse move: dx=%d, dy=%d\n", mouse.dx, mouse.dy);
@@ -40,15 +63,26 @@ int main(void)
                 printf("Middle button pressed\n");
             }
             if (mouse.middle_button_released) {
-                break;
+                running = false;
             }
             if (mouse.middle_button_state) {
                 printf("Middle button held\n");
             }
         }
-
-        usleep(10000);
+        usleep(1000);
     }
+    return NULL;
+}
 
-    return 0;
+void *test_thread_func(void *arg)
+{
+    while (running) {
+        usleep(5000000);
+        pthread_mutex_lock(&mouse_lock);
+        printf("Locking mouse thread.\n");
+        usleep(5000000);
+        pthread_mutex_unlock(&mouse_lock);
+        printf("Mouse thread unlocked.\n");
+    }
+    return NULL;
 }
